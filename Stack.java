0cdb2e7535e26613a4bc1624d7becdec1225af2f
AdorableNewYs <30628793+AdorableNewYs@users.noreply.github.com>
2018-08-16 12:15:59 +0800

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,59 @@
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item>{
+
+    private class Node{
+        Item item;
+        Node next;
+    }
+
+    private Node top;
+    private int N;
+
+    public boolean isEmpty(){return top == null;}
+
+    public Item pop(){
+        if(isEmpty()){
+            return null;
+        }
+        Node ret = top;
+        top = top.next;
+        N--;
+        return ret.item;
+    }
+
+    public void push(Item s){
+        N++;
+        Node oldTop = top;
+        top = new Node();
+        top.item = s;
+        top.next = oldTop;
+    }
+
+    public int size(){ return N; }
+
+    public Iterator<Item> iterator(){
+        return new StackIterator();
+    }
+
+    private class StackIterator implements Iterator<Item>{
+
+        private Node a = top;
+
+        @Override
+        public boolean hasNext() { return a != null;}
+
+        @Override
+        public Item next() {
+            Item res = a.item;
+            a = a.next;
+            return res;
+        }
+        
+        @Override
+        public void remove() {}
+        
+    }
+
+
+}
